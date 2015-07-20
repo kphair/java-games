bf5d7435590adc4bf3527f71203c717954e36006
Kevin Phair <kevin.phair@gmail.com>
2015-07-21 00:29:00 +0100

    Added an "insert coin" control

--- Java Games/src/nu/xdi/games/spaceinvaders/Controls.java
+++ Java Games/src/nu/xdi/games/spaceinvaders/Controls.java
@@ -15,13 +15,15 @@ public class Controls {
 	private static final int RIGHT = 1;
 	private static final int FIRE = 2;
 	private static final int P1_START = 3;
-	private static final int QUIT = 4;
+	private static final int COIN = 4;
+	private static final int QUIT = 5;
 	
 	private static boolean keyStats[] = {
 			false,		// Left 
 			false, 		// Right
 			false, 		// Fire
 			false,		// Player 1 start
+			false,		// Insert coin
 			false		// Quit game
 			};
 
@@ -38,6 +40,7 @@ public class Controls {
 			case KeyEvent.VK_RIGHT: keyStats[RIGHT] = true; break;
 			case KeyEvent.VK_SPACE: keyStats[FIRE] = true; break;
 			case KeyEvent.VK_1: keyStats[P1_START] = true; break;
+			case KeyEvent.VK_5: keyStats[COIN] = true; break;
 			case KeyEvent.VK_ESCAPE: keyStats[QUIT] = true; break;
 		}
 	}
@@ -55,6 +58,7 @@ public class Controls {
 			case KeyEvent.VK_RIGHT: keyStats[RIGHT] = false; break;
 			case KeyEvent.VK_SPACE: keyStats[FIRE] = false; break;
 			case KeyEvent.VK_1: keyStats[P1_START] = false; break;
+			case KeyEvent.VK_5: keyStats[COIN] = false; break;
 			case KeyEvent.VK_ESCAPE: keyStats[QUIT] = false; break;
 		}
 	}
@@ -70,6 +74,9 @@ public class Controls {
 	}
 	public static boolean getP1Start() { 
 		return keyStats[P1_START];
+	}
+	public static boolean getCoin() { 
+		return keyStats[COIN];
 	}
 	public static boolean getQuit() { 
 		return keyStats[QUIT];
