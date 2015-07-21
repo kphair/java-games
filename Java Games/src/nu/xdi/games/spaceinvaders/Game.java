7c967c1f8a17bf7482d02af45c2b10ebb18c2750
Kevin Phair <kevin.phair@gmail.com>
2015-07-21 23:07:13 +0100

    Moved much of the sprite init into main and added end of wave handling

--- Java Games/src/nu/xdi/games/spaceinvaders/Game.java
+++ Java Games/src/nu/xdi/games/spaceinvaders/Game.java
@@ -6,11 +6,12 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.util.Random;
 
+import org.omg.CORBA.FREE_MEM;
+
 import nu.xdi.graphics.util.Images;
 
 /**
  * Game function code
- * 
  * This class manages the tracking and updating of all game objects
 
  * @author Kevin Phair
@@ -20,87 +21,73 @@ public class Game {
 	private static int baseX = 240;
 	private static int shotX, shotY;
 	private static int shotExplode = 0;
-	private static int saucerX;
 	private static int playerShotCount;
 	
 	private static int direction = 0;
 	private static boolean changeDirection = false;
 	private static boolean moveDown = false;
-	private static Invader[] invaders = new Invader[55];
-	private static boolean gameRunning = false;
 	private static int currentRow = 4;
 	private static int currentCol = 0;
+	static Invader[] invaders = new Invader[55];
+	static boolean gameRunning = false;
 
-	private static BufferedImage spriteSheet;
+	static BufferedImage spriteSheet;
 	
-	public  static BufferedImage shotExplosion;
-	private static BufferedImage cheekySprite;
-	private static BufferedImage base;
-	private static BufferedImage missileHit;
-	public static BufferedImage shield;
+	static BufferedImage cheekySprite;
+	static BufferedImage shotExplosion;
+	static BufferedImage base;
+	static BufferedImage missileExplosion;
+	static BufferedImage shield;
 	
-	private static BufferedImage saucer;
-	private static BufferedImage saucerExplosion;
+	static BufferedImage saucer;
+	static BufferedImage saucerExplosion;
+	private static int saucerX;
+	private static int saucerTimer;
+	private static int saucerScore = 50;
+	
 
-	private static int credits;
-	private static int score1;
-	private static int score2;
-	private static int hiScore;
-	private static int lives;
-	private static Missile missile;
+	static Missile missile;
+	private static int credits = 0;
+	private static int score1 = 0;
+	private static int score2 = 0;
+	private static int hiScore = 0;
+	private static int lives1 = 3;
+	private static int lives2 = 3;
+	private static int baseInactive = 0;
+	private static int waveEndTimer = 0;
 	
 	/**
 	 * Start the game by resetting the score and initialising a new array of invaders
-	 * 
 	 */
 	public static void StartGame() {
 
-		spriteSheet = new Images().loadImage("/nu/xdi/games/spaceinvaders/SpaceInvadersSpriteSheet.png");
-		if (spriteSheet == null) {
-			System.out.println("Sprite Sheet not available!");
-			return;
-		}
-
-		cheekySprite = spriteSheet.getSubimage(16, 16, 32, 8);
-		base = spriteSheet.getSubimage(0, 24, 48, 8);
-		shield = spriteSheet.getSubimage(48, 0, 24, 16);
-		
-		shotExplosion = spriteSheet.getSubimage(48, 24, 8, 8);
-		saucer = spriteSheet.getSubimage(56, 24, 16, 8);
-		saucerExplosion = spriteSheet.getSubimage(48, 16, 24, 8);
-
-		baseX = 240;
+		shotX = -1;
+		baseX = 36;
 		shotExplode = 0;
-		saucerX = -1;
 		playerShotCount = 0;
+		saucerScore = 150;
+		saucerX = -1;
 		
+		lives1 = 3;
+		lives2 = 3;
+		score1 = 0;
+		score2 = 0;
+
 		direction = 0;
 		changeDirection = false;
 		moveDown = false;
 		currentRow = 4;
 		currentCol = 0;
-
-		shotX = -1;
-		baseX = 36;
 		
-		lives = 3;
-		score1 = 0;
-		score2 = 0;
-		
-		missile = new Missile(spriteSheet.getSubimage(72, 0, 9, 32));
-
-		new Invader(invaders, spriteSheet.getSubimage(0, 0, 48, 24), spriteSheet.getSubimage(0, 16, 16, 8));
-
 		Game.setDirection(1);
-		
 		Window.drawPlayArea();
-
 		showScores();
 		showCredits();
-		showLives();
-		
-		
+		showLives(lives1);
+		baseInactive = 120;
+		waveEndTimer = 0;
 		gameRunning = true;
+		
 	}
 	
 	private static boolean checkCollision (int xPos, int yPos) {
@@ -119,23 +106,29 @@ public class Game {
 	
 	/**
 	 * Redraw the enemies, player, barriers, bullets and score.
-	 * 
 	 * @param graphics context of drawable area
 	 */
 	public static void update(Graphics g) {
 		
 		if (gameRunning) {
 
+			
 			// Draw player base
-			g.drawImage(base.getSubimage(0, 0, 16, 8), baseX, 432, 32, 16, null);
+			if (baseInactive == 0) {
+				g.drawImage(base.getSubimage(0, 0, 16, 8), baseX, 432, 32, 16, null);
+			} else {
+				baseInactive--;
+			}
 
+			// TASK: Extract this block out into its own class
+			
 			// If player's shot is active (x is positive) then redraw it
 			if (shotX >= 0) {
 				if (shotExplode > 0) {
-					g.drawImage(shotExplosion, shotX, shotY, 16, 16, null);
+					g.drawImage(shotExplosion, shotX, shotY + 4, 16, 16, null);
 					if (--shotExplode == 0) {
 						g.setXORMode(Color.BLACK);
-						g.drawImage(shotExplosion, shotX, shotY, 16, 16, null);
+						g.drawImage(shotExplosion, shotX, shotY + 4, 16, 16, null);
 						g.setPaintMode();
 						shotX = -2;					// Set to -2. See movement() further down
 					}
@@ -157,7 +150,7 @@ public class Game {
 						for (Invader inv : invaders ) {
 							if (inv.getType() > 0) {
 								// is it within the hitbox?
-								if (shotX >= inv.getX() - 2 && shotX <= inv.getX() + 22) {
+								if (shotX >= inv.getX() - 2 && shotX <= inv.getX() + 24) {
 									if (shotY >= inv.getY() && shotY <= inv.getY() + 14) {
 										inv.setExplode();
 										shotX = -2;
@@ -192,7 +185,29 @@ public class Game {
 			// Move to the next missile slot
 			missile.tick();
 		
-			marchInvaders(g);
+			if (marchInvaders(g) == false && waveEndTimer == 0) {
+				waveEndTimer = 60;
+			} else if (waveEndTimer > 0) {
+				System.out.println(waveEndTimer);
+				if (--waveEndTimer == 0) {
+					System.out.println("New wave");
+					
+					missile = new Missile(spriteSheet.getSubimage(72, 0, 9, 32));
+					new Invader();
+					Game.setDirection(1);
+					Window.drawPlayArea();
+					showScores();
+					showCredits();
+					showLives(lives1);
+					saucerScore = 50;
+					playerShotCount = 0;
+					changeDirection = false;
+					moveDown = false;
+					currentRow = 4;
+					currentCol = 0;
+				}
+			}
+			
 		}
 	}
 
@@ -201,7 +216,7 @@ public class Game {
 	 * Step from left to right across the current row of invaders
 	 * moving up one row if the end is reached
 	 */
-	public static void marchInvaders(Graphics g) {
+	public static boolean marchInvaders(Graphics g) {
 		int i;
 		int activeCount = 0;
 		
@@ -218,15 +233,9 @@ public class Game {
 				}
 				break;
 			}
-			for (Invader inv : invaders) {
-				if (inv.getType() > 0 && inv.getExplode() == 0) {
-					activeCount++;
-				}
-			}
-			if (activeCount == 0) {
-				gameRunning = false;
-				System.out.println("");
-			}
+
+			if (invadersLeft(invaders) == 0) return false;
+
 			// If the current invader is active, move it and give it chance to shoot
 			if (currentInv.getType() > 0) {
 				currentInv.moveAcross();
@@ -277,37 +286,64 @@ public class Game {
 			} // end of active invader's activity
 			if (currentInv.getType() > 0) break;
 		}
+		return true;
+	}
+
+	/**
+	 * Counts up the remaining invaders on the screen
+	 * Includes invaders which are in the process of blowing up
+	 * @param invader array
+	 * @return number of invaders
+	 */
+	private static int invadersLeft(Invader[] invaders) {
+		int activeCount = 0;
+		
+		for (Invader inv : invaders) {
+			if (inv.getType() > 0 && inv.getExplode() == 0) {
+				activeCount++;
+			}
+		}
+		return activeCount;
 	}
 
 
 	/**
 	 * Update movement of the player and their projectile (if active)
-	 * 
 	 * @param window that contains the game
 	 */
 	public static void movement(Container window) {
 
-		if (Controls.getLeft() && baseX > 36) baseX -= 2;								// LEFT
-		if (Controls.getRight() && baseX < 372) baseX += 2;		// RIGHT
-		// Fire key has to be released before another shot can be fired.
-		if (!Controls.getFire() && shotX == -2) {
-			shotX = -1;
-		}
-		if (Controls.getFire() && shotX == -1) {
-			shotX = baseX + 10	;
-			shotY = 432;
+		if (baseInactive == 0) {
+			if (Controls.getLeft() && baseX > 36) baseX -= 2;								// LEFT
+			if (Controls.getRight() && baseX < 372) baseX += 2;		// RIGHT
+
+			// Fire key has to be released before another shot can be fired.
+			if (!Controls.getFire() && shotX == -2) {
+				shotX = -1;
+			}
+			if (Controls.getFire() && shotX == -1 && Invader.getFreeze() == 0) {
+				shotX = baseX + 10	;
+				shotY = 432;
+				
+				playerShotCount++;
+				if (playerShotCount == 14) {
+					switch (saucerScore) {
+						case 50: saucerScore = 100; break;
+						
+					}
+					
+				}
+			}
 		}
 	}
 
 	
 	/**
 	 * Set the direction of the Invaders
-	 * 
 	 * When the direction is +/-2 (moving down and right/left) it will be changed to +/-1 when
 	 * the entire squadron has been redrawn
 	 * 
 	 * @param newDirection - -2 = moving down and left, -1 = left, 1 = right, 2 = moving down and right
-	 * 
 	 */
 	public static void setDirection(int newDirection) {
 		direction = newDirection;
@@ -336,37 +372,64 @@ public class Game {
 	
 	/**
 	 * Get the direction of the invaders
-	 * 
 	 * @return -2 = moving down and left, -1 = left, 1 = right, 2 = moving down and right
 	 */
 	public static int getDirection() {
 		return direction;
 	}
-	
+
+	/**
+	 * Get the value of player 1's score
+	 * @return player 1 score
+	 */
 	public static int getScore1() {
 		return score1;
 	}
-	
+	/**
+	 * Get the value of player 2's score
+	 * @return player 2 score
+	 */
 	public static int getScore2() {
 		return score2;
 	}
-	
+	/**
+	 * Get the value of the current high score
+	 * @return current high score
+	 */
 	public static int getHiSCore() {
 		return hiScore;
 	}
-	
+	/**
+	 * Get number of credits in machine
+	 * @return number of credits
+	 */
 	public static int getCredits() {
 		return credits;
 	}
-
+	/**
+	 * Add a credit (insert coin)
+	 */
 	public static void addCredit() {
 		credits++;
 	}
-	
-	public static int getLives() {
-		return lives;
+	/**
+	 * Get the number of lives left for player 1
+	 * @return player 1 lives left
+	 */
+	public static int getLives1() {
+		return lives1;
+	}
+	/**
+	 * Get the number of lives left for player 2
+	 * @return player 2 lives left
+	 */
+	public static int getLives2() {
+		return lives2;
 	}
 	
+	/**
+	 * Update the score display
+	 */
 	public static void showScores() {
 		Graphics g = Window.getPlayAreaGraphics();
 		g.setColor(Color.BLACK);
@@ -378,15 +441,24 @@ public class Game {
 		Text.print(g, 176, 48, String.format("%04d", hiScore));
 		Text.print(g, 336, 48, String.format("%04d", score2));
 	}
-	
+	/**
+	 * Update the credits display
+	 */
 	public static void showCredits() {
 		Graphics g = Window.getPlayAreaGraphics();
+		g.setColor(Color.BLACK);
+		g.fillRect(392, 480, 32, 16);
 		g.setColor(Color.GREEN);
 		Text.print(g, 392, 480, String.format("%02d", getCredits()));
 	}
-	
-	public static void showLives() {
+	/**
+	 * Update the lives display
+	 * @param number of lives to display
+	 */
+	public static void showLives(int lives) {
 		Graphics g = Window.getPlayAreaGraphics();
+		g.setColor(Color.BLACK);
+		g.fillRect(16, 480, 374, 16);
 		g.setColor(Color.GREEN);
 		Text.print(g, 16, 480, String.format("%d", lives));
 		if (lives > 1) {
