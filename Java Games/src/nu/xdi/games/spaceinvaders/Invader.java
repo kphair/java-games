d42a75c32d6ec6f77f6e485799155e925be59549
Kevin Phair <kevin.phair@gmail.com>
2015-07-23 23:29:07 +0100

    Moved in the remaining invader handling methods from Game class

--- Java Games/src/nu/xdi/games/spaceinvaders/Invader.java
+++ Java Games/src/nu/xdi/games/spaceinvaders/Invader.java
@@ -3,6 +3,7 @@ package nu.xdi.games.spaceinvaders;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.Random;
 
 /**
  * Invader class to handle the properties of each invader on the screen
@@ -17,6 +18,17 @@ public class Invader {
 	private int animFrame = 0;
 	private int explode = 0;
 	private BufferedImage image;
+
+	private static int direction = 1;
+	private static boolean changeDirection = false;
+	private static boolean moveDown = false;
+	private static int currentRow = 4;
+	private static int currentCol = 0;
+	static Invader[] invaders = new Invader[55];
+	private static int invaderStep = 0;					// Resets at 60
+	private static int stepSound = 0;					// loops around from 0-3
+	
+	
 	static BufferedImage sprites = null;
 	static BufferedImage explosion = null;
 	static BufferedImage highInv = null;
@@ -50,24 +62,32 @@ public class Invader {
 	public Invader () {
 
 		BufferedImage image = null;
-		Invader[] a = Game.invaders;
+
+		setDirection (1);
+		changeDirection = false;
+		moveDown = false;
+		currentRow = 4;
+		currentCol = 0;
+		invaderStep = 0;
+		stepSound = 1;
+		freeze = 0;
 		
-			System.out.println("Initialising new invaders array");
+		System.out.println("Initialising new invaders array");
 		if (highInv == null) {
 			highInv = sprites.getSubimage(32, 0, 16, 16);
 			medInv = sprites.getSubimage(16, 0, 16, 16); 
 			lowInv = sprites.getSubimage(0, 0, 16, 16);
 		}
 
+		Invader[] a = invaders;
 		for (int j = 0; j < 11; ++j) {
-			a[j] = new Invader(40 + j * 32, 128, 1, highInv);
-			a[j + 11] = new Invader(40 + j * 32, 128 + 32, 2, medInv);
-			a[j + 22] = new Invader(40 + j * 32, 128 + 64, 3, medInv);
-			a[j + 33] = new Invader(40 + j * 32, 128 + 96, 4, lowInv);
-			a[j + 44] = new Invader(40 + j * 32, 128 + 128, 5, lowInv);
+			a[j] = new Invader(40 + j * 32, Game.currentWave * 32 + 128, 1, highInv);
+			a[j + 11] = new Invader(40 + j * 32, Game.currentWave * 32 + 128 + 32, 2, medInv);
+			a[j + 22] = new Invader(40 + j * 32, Game.currentWave * 32 + 128 + 64, 3, medInv);
+			a[j + 33] = new Invader(40 + j * 32, Game.currentWave * 32 + 128 + 96, 4, lowInv);
+			a[j + 44] = new Invader(40 + j * 32, Game.currentWave * 32 + 128 + 128, 5, lowInv);
 		}
 		
-		freeze = 0;
 	}
 	
 	/**
@@ -113,6 +133,148 @@ public class Invader {
 			}
 		}
 	}
+
+	/**
+	 * Step from left to right across the current row of invaders
+	 * moving up one row if the end is reached
+	 */
+	public static boolean marchInvaders(Graphics g) {
+		int i;
+		int activeCount = 0;
+		
+		while (true) {
+			if (++invaderStep == 72) {
+				invaderStep = 0;
+				Sound.steps[stepSound].stop();
+				if (++stepSound == 4) stepSound = 0;
+				Sound.steps[stepSound].play();
+			}
+			
+			Invader currentInv = invaders[currentRow * 11 + currentCol];
+			if (currentInv.getFreeze() > 0) {
+				for (Invader inv : invaders) {
+					if (inv.getType() > 0 && inv.getExplode() == 0) {
+						activeCount++;
+					}
+					if (inv.getExplode() > 0) {
+						inv.redraw(g);
+					}
+				}
+				break;
+			}
+
+			if (invadersLeft(invaders) == 0) return false;
+
+			// If the current invader is active, move it and give it chance to shoot
+			if (currentInv.getType() > 0) {
+				currentInv.moveAcross();
+				if (moveDown) {
+					currentInv.undraw(g);
+					currentInv.moveDown();
+				}
+				currentInv.redraw(g);
+
+				// Give the current invader a chance to fire
+				if (!Game.isBaseInactive() && new Random().nextInt(100) > 90) {
+					// Scan down to make sure it isn't above another invader
+					for (i = currentRow; i <= 4; ++i)  {
+						if (i == 4) break;
+						if (invaders[(i + 1) * 11 + currentCol].getType() > 0) {
+							break;
+						}	
+					}
+					// if i == 4 then all clear. Use the next free missile slot if one available
+					if (i == 4) {
+						Game.missile.dropMissile(currentInv.getX() + 14, currentInv.getY() + 32);
+					}
+				}
+			}
+			// Move to the next invader in the array
+			currentCol++;
+			if (currentCol > 10) {
+				currentCol = 0;
+				currentRow--;
+				/*
+				 * If all the invaders are done check to see if the changeDirection flag is set
+				 */
+				if (currentRow < 0) {
+					currentRow = 4;
+					/*
+					 * Check to see if the direction of the invaders needs to be changed
+					 * and if they need to be told they can move down 
+					 */
+					if (changeDirection) {
+						direction = -direction;
+						currentCol = 0;
+						changeDirection = false;
+						moveDown = true;
+					} else {
+						moveDown = false;
+					}
+				}
+			} // end of active invader's activity
+			if (currentInv.getType() > 0) break;
+		}
+		return true;
+	}
+
+	/**
+	 * Counts up the remaining invaders on the screen
+	 * Includes invaders which are in the process of blowing up
+	 * @param invader array
+	 * @return number of invaders
+	 */
+	private static int invadersLeft(Invader[] invaders) {
+		int activeCount = 0;
+		
+		for (Invader inv : invaders) {
+			if (inv.getType() > 0 && inv.getExplode() == 0) {
+				activeCount++;
+			}
+		}
+		return activeCount;
+	}
+
+	/**
+	 * Set the direction of the Invaders
+	 * When the direction is +/-2 (moving down and right/left) it will be changed to +/-1 when
+	 * the entire squadron has been redrawn
+	 * 
+	 * @param newDirection - -2 = moving down and left, -1 = left, 1 = right, 2 = moving down and right
+	 */
+	public static void setDirection(int newDirection) {
+		direction = newDirection;
+	}
+
+	/**
+	 * Sets the changeDirection flag
+	 * This tells the game that before going around do move all the invaders again it should
+	 * check to see if the direction is to be changed.
+	 */
+	public static void changeDirection() {
+		changeDirection = true;
+	}
+
+	public static boolean getChangeDirection() {
+		return changeDirection;
+	}
+	/**
+	 * This returns the status of the moveDown flag. This is set at the end of a full movement update
+	 * if the changeDirection flag is set.
+	 * @return
+	 */
+	public static boolean getMoveDown() {
+		return moveDown;
+	}
+	
+	/**
+	 * Get the direction of the invaders
+	 * @return -2 = moving down and left, -1 = left, 1 = right, 2 = moving down and right
+	 */
+	public static int getDirection() {
+		return direction;
+	}
+
 	
 	/**
 	 * Clear the position where the invader is before it moves down one line
@@ -138,10 +300,10 @@ public class Invader {
 	 */
 	public void moveAcross() {
 	
-		if (type > 0 && ((x < 22 && Game.getDirection() < 0) || (x > 392 && Game.getDirection() > 0))) {
-			Game.changeDirection();
+		if (type > 0 && ((x < 22 && getDirection() < 0) || (x > 392 && getDirection() > 0))) {
+			changeDirection();
 		}
-		x += Math.signum(Game.getDirection()) * 4;
+		x += Math.signum(getDirection()) * 4;
 		animFrame = (++animFrame % 2);
 	}
 	
@@ -192,6 +354,7 @@ public class Invader {
 	public void setExplode() {
 		explode = 16;
 		freeze++;
+		Sound.invaderExplosion.play();
 	}
 	
 	public static int getFreeze() {
